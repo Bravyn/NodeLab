08875428a0f14b2b87c3c030bc5279c2828b4da1
Ian Bravyn <ianbravynsa@gmail.com>
2024-02-24 18:41:10 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+//responds with hello world for all queries
+
+const server = http.createServer((req, res) => {
+    //set response headers, status code and content type
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Hello World!\n")
+
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
